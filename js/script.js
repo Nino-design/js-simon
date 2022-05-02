3299dc410e197a7d9f654f73d67b3b1c91985d49
Nino-design <npompili@gmail.com>
2022-05-02 10:14:46 +0200

    modifica variabili

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
-const arrayLength = 5
-const randomArray = []
+const arrayLength = 5;
+const randomArray = [];
 for(let i = 0; i < arrayLength; i++) {
     randomArray.push(Math.floor(Math.random() * 100) + 1);
     
@@ -13,10 +13,10 @@ for(let i = 0; i < arrayLength; i++) {
    const arrayNumbers = document.getElementById("numbers");
    arrayNumbers.innerHTML = randomArray;
 
-    var timeLeft = 5;
-    var element = document.getElementById('countdown');
+    let timeLeft = 5;
+    let element = document.getElementById('countdown');
     
-    var timerId = setInterval(countdown, 1000);
+   let timerId = setInterval(countdown, 1000);
     
     function countdown() {
       if (timeLeft == -1) {
@@ -30,9 +30,9 @@ for(let i = 0; i < arrayLength; i++) {
         timeLeft--;
       }
     }
-
+    
     setTimeout(function() {
-         for(let i = 1; i <= 5; i++){
-             prompt('inserisci i numeri visti precedentemente');
-         }
-      }, 7300)
+      for(let i = 1; i <= 5; i++){
+          prompt('inserisci i numeri visti precedentemente');
+      }
+   }, 7300)
